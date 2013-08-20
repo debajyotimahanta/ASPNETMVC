66ed06858b5b69ac4ec0c8484b77999c6b6a8d47
debajyotimahanta <debajyoti.mahanta@gmail.com>
2013-08-20 17:33:34 -0500

    First Commit

---
+++ Aon.MyAon.Web.Tests/HelloWorldTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aon.MyAon.Web.Controllers;
+using System.Web.Mvc;
+
+namespace Aon.MyAon.Web.Tests
+{
+    [TestClass]
+    public class HelloWorldTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            HomeController controller = new HomeController();
+            ViewResult result = controller.Index() as ViewResult;
+            Assert.AreEqual("Hello World", result.ViewBag.Message);
+
+        }
+    }
+}
---
+++ Aon.MyAon.Web/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aon.MyAon.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello World";
+            return View();
+        }
+
+    }
+}
