9e2f35c62f9ffac60135d41feaf297ce849fb4bb
debajyotimahanta <debajyoti.mahanta@gmail.com>
2013-09-05 15:24:39 -0500

    Added twitter bootstrap

--- Aon.MyAon.Web/App_Start/BundleConfig.cs
+++ Aon.MyAon.Web/App_Start/BundleConfig.cs
@@ -38,6 +38,9 @@ namespace Aon.MyAon.Web
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
                         "~/Content/themes/base/jquery.ui.theme.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+     "~/Scripts/bootstrap*"));
         }
     }
 }
